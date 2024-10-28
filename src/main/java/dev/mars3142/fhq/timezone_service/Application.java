f5e876db25002a88443a7f541abfe43bbfcb52ec
Peter Siegmund <developer@mars3142.org>
2024-10-28 14:47:09 +0100

    configure TLS versions
    Signed-off-by: Peter Siegmund <developer@mars3142.org>

--- src/main/java/dev/mars3142/fhq/timezone_service/Application.java
+++ src/main/java/dev/mars3142/fhq/timezone_service/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+    System.setProperty("https.protocols", "TLSv1 TLSv1.1 TLSv1.2 TLSv1.3");
     SpringApplication.run(Application.class, args);
   }
 
